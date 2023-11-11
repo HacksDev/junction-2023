48f1b776ed8c635f7545b8f4ddcbdaf4a84afb28
vladimir <vovaaksenov99@mail.ru>
2023-11-11 16:42:11 +0200

    added scroll

--- src/components/Messages/ChatBox.tsx
+++ src/components/Messages/ChatBox.tsx
@@ -3,14 +3,22 @@ import Message from "./Message";
 import { useEffect, useRef, useState } from "react";
 
 const ChatBox = (props: { messages: MessageData[] }) => {
-  console.log(props);
+  const chatBoxRef = useRef<HTMLDivElement>(null);
+
+  useEffect(() => {
+    // Scroll to the end of the chat box after messages update
+    if (chatBoxRef.current) {
+      const lastMessage = chatBoxRef.current.lastChild as HTMLElement;
+      lastMessage.scrollIntoView({ behavior: 'smooth' });
+    }
+  }, [props.messages]); // Trigger the effect whenever messages change
 
   return (
-    <div className="containerWrap pb-32" style={{ backgroundColor: "#EDF8FA" }}>
-      {props.messages.map((message: MessageData, ind: number) => (
-        <Message key={ind} message={message} />
-      ))}
-    </div>
+      <div className="containerWrap pb-32" style={{ backgroundColor: "#EDF8FA" }} ref={chatBoxRef}>
+        {props.messages.map((message: MessageData, ind: number) => (
+            <Message key={ind} message={message} />
+        ))}
+      </div>
   );
 };
 
