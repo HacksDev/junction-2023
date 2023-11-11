0f68d779ca0c1200f2704203eef7aeff92537a4f
vitaly.v.boytsov <vitaly.v.boytsov@ringcentral.com>
2023-11-11 12:53:22 +0400

    [dev-vb] added folders

---
+++ src/pages/profile/index.tsx
@@ -0,0 +1,13 @@
+import Image from "next/image";
+import { Inter } from "next/font/google";
+
+const inter = Inter({ subsets: ["latin"] });
+
+export default function Home() {
+  return (
+    <div>
+        здесь можно заполнить анкету и получить ссылку для друзей
+        анекта со степером
+    </div>
+  );
+}
