e135efe970c4806964b989560e9f13638b335ee9
vitaly.v.boytsov <vitaly.v.boytsov@ringcentral.com>
2023-11-11 13:37:05 +0400

    [dev-vb] fix

--- src/pages/scenarios/index.tsx
+++ src/pages/scenarios/index.tsx
@@ -4,9 +4,5 @@ import { Inter } from "next/font/google";
 const inter = Inter({ subsets: ["latin"] });
 
 export default function Home() {
-  return (
-    <div>
-        здесь будут все кнопки, которые можно нажать с возможностью их редактирования или отправки
-    </div>
-  );
+  return <div>здесь будут все кнопки, которые можно нажать с возможностью их редактирования или отправки</div>;
 }
