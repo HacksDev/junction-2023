df479f47bb6ea5bec8b51286c87ac784a2883815
vitaly.v.boytsov <vitaly.v.boytsov@ringcentral.com>
2023-11-11 20:01:25 +0400

    [dev-vb] messages

--- src/components/Messages/SendButtonMessage.tsx
+++ src/components/Messages/SendButtonMessage.tsx
@@ -1,4 +1,6 @@
 import { useState } from "react";
+import { useCallback } from "react";
+import { toast } from "react-toastify";
 
 type ButtonState = {
   text: String;
@@ -11,19 +13,19 @@ const SendButtonMessage = (props: { onNewMessage: (message: String, userMessage:
   const [number, setNumber] = useState<number>(0);
   const buttonStates: ButtonState[] = [
     {
-      text: "If you want to get some information, push buttons",
+      text: "If you want to get some additional information, press the button below.",
       buttons_text: ["activities", "usefull link"],
     },
     {
-      text: "If you want to be active, go for a walk",
+      text: "If you want to be active, go for a walk.",
       buttons_text: ["no", "okay"],
     },
     {
-      text: "I see, you don't want to go for a walk, maybe you would like to see movie",
+      text: "I see, you don't want to go for a walk. Maybe you would like to see a movie?",
       buttons_text: ["yes", "no"],
     },
     {
-      text: "Okay, I know some movie for you. It calls 'Garfield'",
+      text: "Good, i know a movie just for you. It is called “Garfield”.",
       buttons_text: ["Good, I will see it", "Brr, it is boring!"],
     },
   ];
@@ -36,6 +38,13 @@ const SendButtonMessage = (props: { onNewMessage: (message: String, userMessage:
     props.onNewMessage(buttonStates[(number + 1) % buttonStates.length].text, false);
   };
 
+  const handleClick = useCallback(() => {
+    toast.info("It is demo conversation, this tree of dialog will be implemented in the future, try another button", {
+      theme: "colored",
+      toastId: 1,
+    });
+  }, []);
+
   return (
     <div className="fixed bottom-0 pt-3 pb-40  w-full flex items-center " style={{ backgroundColor: "#EDF8FA" }}>
       <form
@@ -47,7 +56,7 @@ const SendButtonMessage = (props: { onNewMessage: (message: String, userMessage:
           {buttonStates[number % buttonStates.length].buttons_text[0]}
         </button>
       </form>
-      <button type="submit" className="btn btn-secondary mx-3">
+      <button type="submit" onClick={handleClick} className="btn btn-secondary mx-3">
         {buttonStates[number % buttonStates.length].buttons_text[1]}
       </button>
     </div>
