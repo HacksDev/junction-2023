f1c69d9ffcc117c19aa77ec82a0f28ae0ff8afcc
vitaly.v.boytsov <vitaly.v.boytsov@ringcentral.com>
2023-11-11 18:49:15 +0400

    [dev-vb] messages

--- src/components/Messages/ChatBox.tsx
+++ src/components/Messages/ChatBox.tsx
@@ -9,16 +9,16 @@ const ChatBox = (props: { messages: MessageData[] }) => {
     // Scroll to the end of the chat box after messages update
     if (chatBoxRef.current) {
       const lastMessage = chatBoxRef.current.lastChild as HTMLElement;
-      lastMessage.scrollIntoView({ behavior: 'smooth' });
+      lastMessage.scrollIntoView({ behavior: "smooth" });
     }
   }, [props.messages]); // Trigger the effect whenever messages change
 
   return (
-      <div className="containerWrap pb-32" style={{ backgroundColor: "#EDF8FA" }} ref={chatBoxRef}>
-        {props.messages.map((message: MessageData, ind: number) => (
-            <Message key={ind} message={message} />
-        ))}
-      </div>
+    <div className="containerWrap pb-32" style={{ backgroundColor: "#EDF8FA" }} ref={chatBoxRef}>
+      {props.messages.map((message: MessageData, ind: number) => (
+        <Message key={ind} message={message} />
+      ))}
+    </div>
   );
 };
 
