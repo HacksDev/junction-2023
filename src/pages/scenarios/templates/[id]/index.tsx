9e5263159df580b045bf73523adb48d156b5b81e
vitaly.v.boytsov <vitaly.v.boytsov@ringcentral.com>
2023-11-11 13:38:26 +0400

    [dev-vb] fix

--- src/pages/scenarios/templates/[id]/index.tsx
+++ src/pages/scenarios/templates/[id]/index.tsx
@@ -4,9 +4,5 @@ import { Inter } from "next/font/google";
 const inter = Inter({ subsets: ["latin"] });
 
 export default function Home() {
-  return (
-    <div>
-        здесь можно отредактировать конкретный шаблон и настройки для него, кому отправить и тд
-    </div>
-  );
+  return <div>здесь можно отредактировать конкретный шаблон и настройки для него, кому отправить и тд</div>;
 }
